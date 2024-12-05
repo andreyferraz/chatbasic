5efbde1416d15c74eb65b2237e091378111ce7b4
andreyferraz <andreyferraz@unimedceara.com.br>
2024-12-05 11:14:38 -0300

    class message created

---
+++ src/main/java/com/example/chatbasic/model/Message.java
@@ -0,0 +1,13 @@
+package com.example.chatbasic.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String content;
+    private String sender;
+}
