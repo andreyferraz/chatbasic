367bdaa231b298ceb6faabeb127589cf66caa325
andreyferraz <andreyferraz@unimedceara.com.br>
2024-12-04 21:26:20 -0300

    first packages and configs basic

---
+++ src/main/java/com/example/chatbasic/ChatbasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatbasic/ChatbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
